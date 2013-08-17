442cb8d74fae95f003e7edc8c3d3b77915b94f78
Maksim Pecherskiy <max@maksimize.com>
2013-08-17 18:27:19 -0500

    Adding table view controller for people.

---
+++ AreYouHereYet/AYHTReceiverListTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  AYHTReceiverListTableViewController.h
+//  AreYouHereYet
+//
+//  Created by Maksim Pecherskiy on 8/17/13.
+//  Copyright (c) 2013 Maksim Pecherskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <FlatUIKit/FlatUIKit.h>
+@interface AYHTReceiverListTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSArray *peopleToContact;
+
+@end
